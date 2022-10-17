1249840801cf896bd7c8307fff687dc3ce2e3c06
efriza <Efrizals@it.student.pens.ac.id>
2022-10-17 09:39:50 +0700

    update url api

--- marriage_story_app/lib/const/strings.dart
+++ marriage_story_app/lib/const/strings.dart
@@ -1,2 +1,2 @@
-// final String baseURLAPI = 'https://secret-plains-59809.herokuapp.com/api/';
-final String baseURLAPI = 'http://192.168.1.9:8000/api/';
+final String baseURLAPI = 'https://serene-caverns-05940.herokuapp.com/api/';
+// final String baseURLAPI = 'http://192.168.1.9:8000/api/';
